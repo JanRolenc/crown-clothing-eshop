f8369ef95df72d0a112d6893e7500c19bcbe2b0a
Jan Rolenc <rolenc@dis.ics.muni.cz>
2022-04-26 13:57:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-
 const App = () => {
   const categories = [
     {
       id: 1,
-      img: 'http://',
-      title: 'Hats'
-    }
-  ]
+      img: "http://",
+      title: "Hats",
+    },
+  ];
   return (
-    <div className="categories-container" >
-      {
-        categories.map((category) => (
-          <div className="category-container">
-            <img src={category.img} alt={category.title.toLowerCase()} />
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div className="category-container">
+          <img src={category.img} alt={category.title.toLowerCase()} />
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+            <p>Shop Now</p>
           </div>
-        ))
-      }
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
