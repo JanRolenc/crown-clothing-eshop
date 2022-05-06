828344b2be02e88a68b29c64f62b81c00e4f87a6
Jan Rolenc <jrolenc@centrum.cz>
2022-05-06 16:11:07 +0200

    added sign in component as well as firebase utils

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+
 const SignIn = () => {
+  const logGooleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user)
+  }
   return (
     <div>
-      <h1>im a signin page</h1>;
+      <h1>im a signin page</h1>
+      <button onClick={logGooleUser}>Sign in with Google</button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,29 +4,52 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithGooglePopup,
-  GoogleAuthProvider,
-  signInWithPopup,
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
 } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAC9IzNYFL5-LQi1p8U504Ao6vewZX6r8k",
-  authDomain: "crown-clothing-eshop.firebaseapp.com",
-  projectId: "crown-clothing-eshop",
-  storageBucket: "crown-clothing-eshop.appspot.com",
-  messagingSenderId: "82806236453",
-  appId: "1:82806236453:web:04b9de83ba6ea75b18a6cc",
+    apiKey: "AIzaSyAC9IzNYFL5-LQi1p8U504Ao6vewZX6r8k",
+    authDomain: "crown-clothing-eshop.firebaseapp.com",
+    projectId: "crown-clothing-eshop",
+    storageBucket: "crown-clothing-eshop.appspot.com",
+    messagingSenderId: "82806236453",
+    appId: "1:82806236453:web:04b9de83ba6ea75b18a6cc",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  promt: "select-account",
+    promt: "select-account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid) //pouze reference na misto v databazi
+    console.log(userDocRef);
+
+    const userSnapShot = await getDoc(userDocRef) //tady uz se ptame na skutecna data v referenci
+    console.log(userSnapShot);
+    console.log(userSnapShot.exists()); //vyhodi false, pokud nemame v databazi zaznam
+
+    if (!userSnapShot.exists()) { //pokud user existuje, podminka bude !true, tedy false a nic se zakladat nebude
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt })
+        } catch (error) {
+            console.log("error creating the user", error.message)
+
+        }
+    }
+    return userDocRef;
+}
